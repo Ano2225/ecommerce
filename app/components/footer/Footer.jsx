0e4f36fa14b8ec4493eb9f70054e42f2378f95b6
Ano2225 <ouatt0767@gmail.com>
2024-01-18 05:14:27 +0000

    First Page in loading ( Fetch and Display Products in data not finished)

---
+++ app/components/footer/Footer.jsx
@@ -0,0 +1,57 @@
+import Container from "@/app/Container"
+import FooterList from "./FooterList"
+import Link from "next/link"
+import {MdFacebook} from 'react-icons/md'
+import {AiFillInstagram} from 'react-icons/ai'
+
+const Footer = () => {
+  return (
+   <footer className="bg-slate-700
+   text-slate-200 text-sm mt-16 
+   ">
+    <Container>
+        <div className="flex
+        flex-col md:flex-row
+        justify-between pt-16 pb-8">
+            <FooterList>
+                <h3 className="text-base font-bold mb-2">Nos Categories</h3>
+                <Link href='#'>Habits</Link>
+                <Link href='#'>Chaussures</Link>
+                <Link href='#'>Montres</Link>
+                <Link href='#'>Pantalons</Link>
+                <Link href='#'>Autres</Link>
+            </FooterList>
+            <FooterList>
+                <h3 className="text-base font-bold mb-2">Service Client</h3>
+                <Link href='#'>Nous contacter</Link>
+                <Link href='#'>Retour et remboursement</Link>
+            </FooterList>
+            <div className="w-full md:w-1/3 mb-6 mb:mb-0">
+                <h3 className="text-base font-bold">A propos de nous</h3>
+                <p className="mb-2">
+                    Owen-Market est une boutique de vente en ligne présent depuis 2021.
+                    Avec Owen-Market c&apos;est la qualité à bas prix . <br /> Toujours bien fait
+                </p>
+                <p>&copy;{new Date().getFullYear()} Owen-Market. Tous droits reservés </p>
+            </div>
+            <FooterList>
+                <h3 className="text-base font-bold mb-2">Nous suivre</h3>
+                <div className="flex gap-2">
+                    <Link href="#">
+                        <MdFacebook size={24}/>
+                    </Link>
+                    <Link href="#">
+                        <AiFillInstagram size={24}/>
+                    </Link>
+                </div>
+            </FooterList>
+
+            
+        </div>
+    </Container>
+
+   </footer>
+  )
+}
+
+export default Footer
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,28 @@
+import Container from "./Container";
+import HomeBanner from "./HomeBanner";
+import {products} from "../utils/products"
+import { truncateText } from "@/utils/truncateTexts";
+import ProductCard from "./components/products/ProductCard";
+
 
 export default function Home() {
   return (
-   <main>
-    <h1>Hello</h1>
-   </main>
+   <div>
+    <Container>
+      <div className="p-8">
+        <HomeBanner/>
+      </div>
+      <div className="grid grid-cols-2 
+      sm:grid-cols-3 
+      lg:grid:cols:4
+       xl:grid-cols-5 
+       2xl:grid-cols-6
+        gap-8">
+          {products.map((product: any) => {
+            return <ProductCard key={product.id} data={product}/>
+          })}
+      </div>
+    </Container>
+   </div>
   )
 }
