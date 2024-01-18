a0eeb9ff0301198ad03dc1292ecfc419910a97bd
Ano2225 <ouatt0767@gmail.com>
2024-01-18 00:09:31 +0000

    Setup of the application

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Poppins } from 'next/font/google'
+
+const poppins = Poppins({ subsets: ['latin'], weight:['400','700'] })
+
+export const metadata: Metadata = {
+  title: 'Owen-Market',
+  description: 'E-commerce app',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={poppins.className}>{children}</body>
+    </html>
+  )
+}
