3ad957a3b2fd47407813c7b65015e1e34708eda3
Ano2225 <ouatt0767@gmail.com>
2024-02-08 00:08:46 +0000

    SearchBar Logic Done

--- app/components/nav/NavBar.tsx
+++ app/components/nav/NavBar.tsx
@@ -5,6 +5,7 @@ import CartCount from "./CartCount";
 import UserMenu from "./UserMenu";
 import { getCurrentUser } from "@/actions/getCurrentUser";
 import Categories from "./Categories";
+import SearchBar from "./SearchBar";
 
 const redressed = Redressed({subsets: ['latin'],
 weight:['400']})
@@ -30,7 +31,9 @@ const NavBar = async () => {
             md:gap-0
             ">
                 <Link href="/" className={`${redressed.className} font-bold text-2xl`}>Owen-Market</Link>
-                <div className="hidden md:block">Rechercher</div>
+                <div className="hidden md:block">
+                  <SearchBar/>
+                </div>
                 <div className="flex items-center 
                  gap-8 
                 md:gap-12
