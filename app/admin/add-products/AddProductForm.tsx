581eb23c89771eedb564a919d2368413d2d50ae5
Ano2225 <ouatt0767@gmail.com>
2024-02-09 01:07:09 +0000

    update productForm.jsx

--- app/admin/add-products/AddProductForm.tsx
+++ app/admin/add-products/AddProductForm.tsx
@@ -10,13 +10,15 @@ import Input from "@/app/components/inputs/input";
 import firebaseApp from "@/libs/firebase";
 import { categories } from "@/utils/Categories";
 import {colors} from "@/utils/Colors";
-import { useCallback, useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import toast from "react-hot-toast";
 import {getDownloadURL, getStorage, ref, uploadBytesResumable} from 'firebase/storage';
 import axios from "axios";
 import { useRouter } from "next/navigation";
 import { getCurrentUser } from "@/actions/getCurrentUser";
+import { SafeUser } from "@/types";
+
 
 export type ImageType = {
     color: string;
@@ -30,8 +32,11 @@ export type UploadedImageType = {
     image: string;
 }
 
+interface AddProductFormProps {
+    currentUser: SafeUser;
+  }
 
-const AddProductForm = ({currentUser}) => {
+const AddProductForm:React.FC<AddProductFormProps> = ({currentUser}) => {
 
         
     const router = useRouter();
