260007441e6d22d122d947fdf7af67e1274bb3d6
Ano2225 <ouatt0767@gmail.com>
2024-02-10 21:34:48 +0000

    update page.tsx

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,20 +1,36 @@
 import { getCurrentUser } from "@/actions/getCurrentUser";
 import Container from "../components/Container";
 import CartClient from "./CartClient";
+import { SafeUser } from "@/types";
+import React, { useEffect, useState } from "react";
 
-const Cart = async() => {
+interface CartProps {
+  currentUser: SafeUser | null;
+}
 
-  const currentUser = await getCurrentUser();
+const Cart: React.FC<CartProps> = () => {
+  const [currentUser, setCurrentUser] = useState<SafeUser | null>(null);
 
- 
+  useEffect(() => {
+    const fetchCurrentUser = async () => {
+      try {
+        const user = await getCurrentUser();
+      } catch (error) {
+        console.error("Error fetching current user:", error);
+        setCurrentUser(null);
+      }
+    };
+
+    fetchCurrentUser();
+  }, []);
 
   return (
     <div className="pt-8">
       <Container>
-          <CartClient currentUser={currentUser}/>
+        <CartClient currentUser={currentUser} />
       </Container>
     </div>
-  )
-}
+  );
+};
 
 export default Cart;
