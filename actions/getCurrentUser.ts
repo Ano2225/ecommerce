1cf22ca4eea71ebd2ca737f7cf0f3c50727816a3
Ano2225 <ouatt0767@gmail.com>
2024-02-26 04:11:17 +0000

    .

--- actions/getCurrentUser.ts
+++ actions/getCurrentUser.ts
@@ -33,33 +33,3 @@ export async function getServerSideProps(context) {
         }
     };
 }
-
-
-import { authOptions } from "@/pages/api/auth/[...nextauth]";
-import { getServerSession } from 'next-auth';
-import prisma from '@/libs/prismadb';
-
-export async function getCurrentUser(req) {
-    const session = await getServerSession(authOptions, req);
-
-    const userEmail = session?.user?.email;
-    if (!userEmail) {
-        return null;
-    }
-
-    const currentUser = await prisma.user.findUnique({
-        where: { email: userEmail },
-        include: { orders: true },
-    });
-
-    if (!currentUser) {
-        return null;
-    }
-
-    return {
-        ...currentUser,
-        createdAt: currentUser.createdAt.toISOString(),
-        updatedAt: currentUser.updateAt?.toISOString(),
-        emailVerified: currentUser.emailVerified?.toISOString() || null,
-    };
-}
