db8e80ff37bff7e120ebd67df426d8d688f68546
Ano2225 <ouatt0767@gmail.com>
2024-02-26 04:18:25 +0000

    retryy

--- actions/getCurrentUser.ts
+++ actions/getCurrentUser.ts
@@ -1,40 +1,5 @@
 // @ts-nocheck
 
-
-import { authOptions } from "@/pages/api/auth/[...nextauth]";
-import { getServerSession } from 'next-auth';
-import prisma from '@/libs/prismadb';
-
-export async function getServerSideProps(context) {
-    const session = await getServerSession(authOptions, context.req);
-
-    const userEmail = session?.user?.email;
-    if (!userEmail) {
-        return { props: {} };
-    }
-
-    const currentUser = await prisma.user.findUnique({
-        where: { email: userEmail },
-        include: { orders: true },
-    });
-
-    if (!currentUser) {
-        return { props: {} };
-    }
-
-    return {
-        props: {
-            user: {
-                ...currentUser,
-                createdAt: currentUser.createdAt.toISOString(),
-                updatedAt: currentUser.updateAt?.toISOString(),
-                emailVerified: currentUser.emailVerified?.toISOString() || null,
-            }
-        }
-    };
-}
-
-
 import { authOptions } from "@/pages/api/auth/[...nextauth]";
 import { getServerSession } from 'next-auth';
 import prisma from '@/libs/prismadb';
